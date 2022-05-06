e6aed6296d479f50b5b25c58472b28cf89e493f8
mdraguet <martin.draguet@gmail.com>
2022-05-06 11:20:37 +0200

    Update pyVRP.py

--- src/pyVRP.py
+++ src/pyVRP.py
@@ -131,6 +131,7 @@ def evaluate_time(distance_matrix, parameters, depot, subroute, velocity):
     time       = [0]*len(subroute_j)
     for i in range(0, len(time)):
         time[i] = time[i] + distance_matrix[(subroute_i[i], subroute_j[i])]/velocity[0]
+        print(time[i], tw_early[subroute_j][i])
         if (time[i] < tw_early[subroute_j][i]):
             wait[i] = tw_early[subroute_j][i] - time[i]
             time[i] = tw_early[subroute_j][i]
